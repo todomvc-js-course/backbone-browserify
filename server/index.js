33ccb0545e9780df7bd58e8bd011dbb8bb954530
vitkarpov <viktor.s.karpov@gmail.com>
2016-03-02 01:51:33 +0300

    plug fake db as npm package

--- server/index.js
+++ server/index.js
@@ -5,33 +5,34 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var defaultData = JSON.parse(fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf-8'));
-var fakeDB = new (require('./fake-database.js'))(defaultData);
+var data = JSON.parse(fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf-8'));
+var FakeDB = require('fake-db');
+var db = new FakeDB(data);
 
 app.use(bodyParser.json());
 
 app.get('/api/todos', function(req, res) {
-  res.json(fakeDB.get());
+  db.getCollection().then(function(collection) {
+    res.json(collection);
+  });
 });
 
 app.get('/api/todos/:id', function(req, res) {
-  res.json(fakeDB.get(req.params.id));
+  db.getItem(req.params.id).then(function(item) {
+    req.json(item);
+  });
 });
 
 app.put('/api/todos/:id', function(req, res) {
-  var id = req.params.id;
-  fakeDB.set(id, req.body);
-  res.send();
-});
-
-app.put('/api/todos/:id', function(req, res) {
-  fakeDB.set(req.params.id, req.body);
-  res.send();
+  db.setItem(req.params.id, req.body).then(function() {
+    res.send();
+  });
 });
 
 app['delete']('/api/todos/:id', function(req, res) {
-  fakeDB.remove(req.params.id);
-  res.send();
+  db.removeItem(req.params.id).then(function() {
+    res.send();
+  });
 });
 
 app.use(express.static(path.join(process.cwd(), 'public')));
